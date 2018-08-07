1cdaa42e939557a245ea0c86eb932b177b407c4b
Harvey Zack <laozhaochaguan@hotmail.com>
2018-08-07 22:49:31 +0800

    update docs

--- config.js
+++ config.js
@@ -3,13 +3,16 @@ const path = require("path");
 module.exports = {
   port: 3000,
   hash: true,
-  cdn: "https://zhw2590582.github.io/website-builder/",
+  cdn: "/website-builder/",
   i18n: {
     zh: path.resolve('./src/languages/zh.json'),
     en: path.resolve('./src/languages/en.json'),
     jp: path.resolve('./src/languages/jp.json'),
   },
   htmlReplace: [{
+    pattern: '@@docs',
+    replacement: '/website-builder'
+  }, {
     pattern: '@@email',
     replacement: 'laozhaochaguan@gmail.com'
   }, {
